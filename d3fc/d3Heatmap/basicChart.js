6aa87ce8d40e82d3e4b6cedd78771690b056bfc5
Jordy Yeoman <jordyyeoman@gmail.com>
2022-12-22 17:11:42 +0800

    feat: process chart data and display heatmap

--- d3fc/d3Heatmap/basicChart.js
+++ d3fc/d3Heatmap/basicChart.js
@@ -1,15 +1,15 @@
-const width = 820;
-const height = 384;
+// const width = 820;
+// const height = 384;
 
-const color = d3.scaleOrdinal(d3.schemeCategory10);
+console.log("System starting....");
 
 const pointSeries = fc
   .seriesWebglPoint()
-  .crossValue((d) => d.group)
-  .mainValue((d) => d.variable)
-  .size(4)
+  .crossValue((d) => d.iteration)
+  .mainValue((d) => d.reading)
+  .size(20)
   .decorate((program) => {
-    fc.pointFill().color([1, 0, 10, 1])(program);
+    fc.symbolsFill().color([1, 0, 0, 1])(program);
     fc.pointAntiAlias()(program);
 
     const gl = program.context();
@@ -22,60 +22,58 @@ const pointSeries = fc
     );
   });
 
-const xExtent = fc
-  .extentLinear()
-  .accessors([(d) => d.group])
-  .pad([0.1, 0.1]);
-const yExtent = fc.extentLinear().accessors([(d) => d.variable]);
+const xExtent = fc.extentLinear().accessors([(d) => d.iteration]);
+// .pad([0, 0.01]);
+const yExtent = fc.extentLinear().accessors([(d) => d.reading]);
 
-d3.tsv("basicHeatmapData.csv", type).then((data) => {
+d3.csv("testFormat.csv", type).then((data) => {
   const xScale = d3.scaleLinear().domain(xExtent(data));
   const xScaleCopy = xScale.copy();
   const yScale = d3.scaleLinear().domain(yExtent(data));
   const yScaleCopy = yScale.copy();
 
-  const zoom = d3
-    .zoom()
-    .extent([
-      [0, 0],
-      [width, height],
-    ])
-    .scaleExtent([1, 10])
-    .translateExtent([
-      [0, 0],
-      [width, height],
-    ])
-    .on("zoom", () => {
-      xScale.domain(d3.event.transform.rescaleX(xScaleCopy).domain());
-      yScale.domain(d3.event.transform.rescaleY(yScaleCopy).domain());
-      d3.select("d3fc-group").node().requestRedraw();
-    });
+  // const zoom = d3
+  //   .zoom()
+  //   .extent([
+  //     [0, 0],
+  //     [width, height],
+  //   ])
+  //   .scaleExtent([1, 10])
+  //   .translateExtent([
+  //     [0, 0],
+  //     [width, height],
+  //   ])
+  //   .on("zoom", () => {
+  //     xScale.domain(d3.event.transform.rescaleX(xScaleCopy).domain());
+  //     yScale.domain(d3.event.transform.rescaleY(yScaleCopy).domain());
+  //     d3.select("d3fc-group").node().requestRedraw();
+  //   });
 
-  const decorate = (selection) => {
-    selection
-      .enter()
-      .select(".plot-area")
-      .on("measure.range", () => {
-        xScaleCopy.range([0, d3.event.detail.width]);
-        yScaleCopy.range([d3.event.detail.height, 0]);
-      })
-      .call(zoom);
-  };
+  // const decorate = (selection) => {
+  //   selection
+  //     .enter()
+  //     .select(".plot-area")
+  //     .on("measure.range", () => {
+  //       xScaleCopy.range([0, d3.event.detail.width]);
+  //       yScaleCopy.range([d3.event.detail.height, 0]);
+  //     });
+  //   .call(zoom);
+  // };
 
   const chart = fc
     .chartCartesian(xScale, yScale)
     .webglPlotArea(pointSeries)
-    .xLabel("Count")
-    .yLabel("Sensor Value")
-    .yTickFormat(d3.format(".3s"))
-    .decorate(decorate);
+    .xLabel("Iteration")
+    .yLabel("Reading");
+  // .yTickFormat(d3.format(".3s"))
+  // .decorate(decorate); // Remove zoom decoration for now to improve rendering performance
 
   d3.select("#chart").datum(data).call(chart);
 });
 
 function type(d) {
-  d.group = Number(d.group);
-  d.variable = Number(d.variable);
-  d.value = Number(d.value);
+  d.sensor = Number(d.sensor);
+  d.reading = Number(d.reading);
+  d.iteration = Number(d.iteration);
   return d;
 }
