2cb8ef8d892603a1178402a98aea51b2945f535c
Jordy Yeoman <jordyyeoman@gmail.com>
2023-01-01 20:37:43 +0800

    chore: Adding colour points to chart values still a WIP

--- d3fc/d3Heatmap/basicChart.js
+++ d3fc/d3Heatmap/basicChart.js
@@ -1,54 +1,125 @@
-// const width = 820;
-// const height = 384;
+// // const width = 820;
+// // const height = 384;
 
-console.log('System starting....');
+// // Examples of color
+// // Full blue color with 50% opacity
+// // fc.pointFill().color([0, 0, 1, 0.5])(program);
 
+// // Full green color with 75% opacity
+// // fc.pointFill().color([0, 1, 0, 0.75])(program);
+
+// // Full yellow color with 100% opacity
+// // fc.pointFill().color([1, 1, 0, 1])(program);
+
+// console.log('System starting....');
+
+// const xExtent = fc
+//   .extentLinear()
+//   .accessors([(d) => d.iteration])
+//   .pad([0.001, 0.001]);
+// const yExtent = fc.extentLinear().accessors([(d) => d.reading]);
+
+// d3.csv('./utils/PROCESSED_BeltThickness.csv', type).then((data) => {
+//   // Find domain values
+//   const minValue = d3.min(data, (d) => d.reading);
+//   const maxValue = d3.max(data, (d) => d.reading);
+
+//   // const colorScale = (d) => {
+//   //   console.log('data: ', d);
+//   //   const color = d3.hsl(Math.random() * 360, 0.5, 0.5);
+//   //   console.log(color);
+//   //   return color;
+//   // };
+//   const colorScale = d3
+//     .scaleSequential(fc.interpolateRgb)
+//     .domain([minValue, maxValue]);
+
+//   const xScale = d3.scaleLinear().domain(xExtent(data));
+//   // Hardcoded value to set the x-range and add padding
+//   // const xScale = d3.scaleLinear().domain([0, 250]);
+//   const yScale = d3.scaleLinear().domain(yExtent(data));
+//   // const yScale = d3.scaleLinear().domain([20, 25]);
+
+//   const getRandomColor = () => {
+//     return [Math.random(), Math.random(), Math.random(), Math.random()];
+//   };
+
+//   const pointSeries = fc
+//     .seriesWebglPoint()
+//     .crossValue((d) => d.iteration)
+//     .mainValue((d) => d.reading)
+//     .size(20)
+//     .decorate((program) => {
+//       // fc.pointFill().color(getRandomColor())(program);
+//       // fc.pointFill().color([0, 0, 1, 0.5])(program);
+//       console.log('Calling pointFill bra');
+//       // fc.pointFill((d) => colorScale(d.reading))(program);
+//       // fc.pointFill((d) => colorScale(d.reading))(program);
+//       console.log('End pointfill');
+
+//       fc.pointAntiAlias()(program);
+
+//       const gl = program.context();
+//       gl.enable(gl.BLEND);
+//       gl.blendFuncSeparate(
+//         gl.SRC_ALPHA,
+//         gl.ONE_MINUS_DST_ALPHA,
+//         gl.ONE,
+//         gl.ONE_MINUS_SRC_ALPHA
+//       );
+//     });
+
+//   const chart = fc
+//     .chartCartesian(xScale, yScale)
+//     .webglPlotArea(pointSeries)
+//     .xLabel('Iteration')
+//     .yLabel('Reading');
+
+//   d3.select('#chart').datum(data).call(chart);
+// });
+
+// function type(d) {
+//   d.sensor = Number(d.sensor);
+//   d.reading = Number(d.reading);
+//   d.iteration = Number(d.iteration);
+//   return d;
+// }
 const xExtent = fc.extentLinear().accessors([(d) => d.iteration]);
-// .pad([0, 0.01]);
 const yExtent = fc.extentLinear().accessors([(d) => d.reading]);
 
 d3.csv('./utils/PROCESSED_BeltThickness.csv', type).then((data) => {
-  // Find domain values
+  // Find the minimum and maximum values
   const minValue = d3.min(data, (d) => d.reading);
   const maxValue = d3.max(data, (d) => d.reading);
 
-  // Generate a color scale
+  // Create a color scale
   const colorScale = d3
     .scaleSequential(fc.interpolateRgb)
     .domain([minValue, maxValue]);
 
+  // Create the x and y scales
   const xScale = d3.scaleLinear().domain(xExtent(data));
-  const xScaleCopy = xScale.copy();
   const yScale = d3.scaleLinear().domain(yExtent(data));
-  const yScaleCopy = yScale.copy();
 
+  // Create the point series
   const pointSeries = fc
     .seriesWebglPoint()
     .crossValue((d) => d.iteration)
     .mainValue((d) => d.reading)
-    .size(20)
+    .size(50)
     .decorate((program) => {
+      // Set the color of the data point
       fc.pointFill().color((d) => colorScale(d.reading))(program);
-      fc.pointAntiAlias()(program);
-
-      const gl = program.context();
-      gl.enable(gl.BLEND);
-      gl.blendFuncSeparate(
-        gl.SRC_ALPHA,
-        gl.ONE_MINUS_DST_ALPHA,
-        gl.ONE,
-        gl.ONE_MINUS_SRC_ALPHA
-      );
     });
 
+  // Create the chart
   const chart = fc
     .chartCartesian(xScale, yScale)
     .webglPlotArea(pointSeries)
     .xLabel('Iteration')
     .yLabel('Reading');
-  // .yTickFormat(d3.format(".3s"))
-  // .decorate(decorate); // Remove zoom decoration for now to improve rendering performance
 
+  // Render the chart
   d3.select('#chart').datum(data).call(chart);
 });
 
